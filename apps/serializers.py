2b34b5b39beac6582f6d34a540ff35b65d81bad7
mrx <meibrohimov@gmail.com>
2025-03-07 00:48:01 +0500

    add new changes

--- apps/serializers.py
+++ apps/serializers.py
@@ -6,4 +6,10 @@ from apps.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'telegram_id', 'role')
+        fields = ('first_name', 'last_name', 'username', 'telegram_id')
+
+    def validate_telegram_id(self, value):
+        """Ensure telegram_id is always required"""
+        if not value:
+            raise serializers.ValidationError("Telegram ID is required.")
+        return value
--- apps/urls.py
+++ apps/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.views import TelegramApiView, TelegramUserListAPIView, TelegramUserDetailAPIView
 
 urlpatterns = [
-    path('auth/tg', TelegramApiView.as_view()),
-    path('auth/users', TelegramUserListAPIView.as_view()),
-    path('auth/users/<int:pk>', TelegramUserDetailAPIView.as_view()),
+    path('auth/tg', TelegramApiView.as_view(),name="telegram-auth"),
+    path('auth/users', TelegramUserListAPIView.as_view(),name="telegram-user-list"),
+    path('auth/users/<int:pk>', TelegramUserDetailAPIView.as_view(),name="telegram-user-detail"),
 ]
