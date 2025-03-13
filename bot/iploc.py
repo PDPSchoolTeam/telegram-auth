41e2a187d0b55c52e2b774b7765d188c446257ee
mrx <meibrohimov@gmail.com>
2025-03-13 10:16:50 +0500

    add new get ip location

--- bot/iploc.py
+++ bot/iploc.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from requests import get
 
 
@@ -7,9 +9,12 @@ def get_lang_lit(ip: str) -> list:
     for x in data.json().items():
         key, val = x
         dataset[key] = val
+    pprint(dataset)
     lan_lat = dataset.get('loc')
     s = lan_lat.split(',')
 
     latitude = s[0]
     longitude = s[1]
     return [latitude, longitude]
+
+get_lang_lit('95.214.211.134')
